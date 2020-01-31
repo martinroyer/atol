706dbac241da6085654fc476657d51c307cfdfdc
martinroyer <martinpierreroyer@gmail.com>
2020-01-31 10:34:45 +0100

    measure weighting and min center dist inertia

--- atol/atol.py
+++ atol/atol.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """
 @author: Martin Royer
-@copyright: INRIA 2019
+@copyright: INRIA 2019-2020
 """
 
 import numpy as np
@@ -21,7 +21,7 @@ def gaus_feats(diags, centers, inertias, eps=1e-10):
 
 def weighting_switch(viewpoint):
     return {
-        "None": lambda _: None, # @todo: fix return because weights will be subscripted later
+        "None": lambda _: None,
         "cloud": lambda diags: np.concatenate([ np.ones(shape=diag.shape[0]) for diag in diags ]),
         "measure": lambda diags: np.concatenate([ np.ones(shape=diag.shape[0]) / diag.shape[0] for diag in diags ]),
     }.get(viewpoint, "None")
@@ -32,7 +32,8 @@ class Atol(BaseEstimator, ClusterMixin, TransformerMixin):
         Read more in [https://arxiv.org/abs/1909.13472]
     """
 
-    def __init__(self, n_centers=5, method=lapl_feats, aggreg=np.sum, viewpoint="None", batch_size=0):
+    def __init__(self, n_centers=5, method=lapl_feats, aggreg=np.sum, viewpoint="None", batch_size=0, n_calib=-1):
+        self.n_calib = n_calib
         self.n_centers = n_centers
         self.r_centers = n_centers
         self.method = method
@@ -50,6 +51,10 @@ class Atol(BaseEstimator, ClusterMixin, TransformerMixin):
         :param diags: list of diagrams from which to learn center locations and cluster spread
         :return: None
         """
+        if self.n_calib > 0:
+            diags = diags[:self.n_calib]
+        elif self.n_calib == 0:
+            diags = np.random.rand(self.n_centers, 2)
         weights = self.weighting_method(diags)
         diags_concat = np.concatenate(diags)
         kmeans = KMeans() if not self.batch_size else MiniBatchKMeans(batch_size=self.batch_size)
@@ -57,9 +62,12 @@ class Atol(BaseEstimator, ClusterMixin, TransformerMixin):
         kmeans.fit(diags_concat, sample_weight=weights)
         labels = np.argmin(pairwise.pairwise_distances(diags_concat, Y=kmeans.cluster_centers_), axis=1)
         self.centers  = np.array([kmeans.cluster_centers_[lab,:] for lab in np.unique(labels)])
-        self.inertias = np.array([[KMeans(n_clusters=1).fit(diags_concat[labels == lab, :],
-                                    sample_weight=weights[labels == lab]).inertia_] for lab in np.unique(labels)])
-        self.r_centers = np.size(np.unique(kmeans.labels_))
+        dist_centers = pairwise.pairwise_distances(self.centers)
+        np.fill_diagonal(dist_centers, np.inf)
+        self.inertias = np.min(dist_centers, axis=0)/2
+        self.r_centers = np.size(np.unique(labels))
+        # self.inertias = np.array([[KMeans(n_clusters=1).fit(diags_concat[labels == lab, :],
+        #                     sample_weight=weights[labels == lab]).inertia_] for lab in np.unique(labels)])
         return self
 
     def transform(self, diag):
@@ -70,5 +78,6 @@ class Atol(BaseEstimator, ClusterMixin, TransformerMixin):
         :param diag: entry diagram to vectorise
         :return: array of size `n_centers`
         """
-        diag_atol = self.aggreg(self.method(diag, self.centers, self.inertias.T), axis=0)
+        weights = self.weighting_method(diag)[0]
+        diag_atol = self.aggreg(self.method(diag, self.centers, self.inertias.T)*weights, axis=0)
         return diag_atol
